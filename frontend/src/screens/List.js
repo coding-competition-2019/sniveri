6d579605d2cdadd55abae60db10549d102aca487
Alan Buzek <alanbuzek@exosite.com>
2019-12-04 13:04:30 +0100

    Grid list improved

--- frontend/src/screens/List.js
+++ frontend/src/screens/List.js
@@ -3,23 +3,14 @@ import mockList from '../misc/mock-places';
 import ListItem from '../components/ListItem';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-
 export default function List() {
-  const itemsList = mockList.places.map(place => {
-    return <ListItem {...place} key={place.name} />;
-  });
-
   return (
-    <Grid container spacing={2}>
-     {mockList.places.map(place => 
-      (<Grid container item xs={12} sm={6} lg={4} spacing={3}>
-        <ListItem {...place} key={place.name} />
-      </Grid>)
-     )}
+    <Grid container spacing={3} style={{ padding: 10, maxWidth: 1400, margin: 'auto' }} justify="center">
+      {mockList.places.map(place => (
+        <Grid container item xs={12} md={6} lg={4} spacing={3} justify="center">
+          <ListItem {...place} key={place.name} />
+        </Grid>
+      ))}
     </Grid>
   );
-
-  // return <div style={{ maxWidth: '1200px', margin: 'auto', padding: 10}}>
-  //   {itemsList}
-  // </div>
 }
