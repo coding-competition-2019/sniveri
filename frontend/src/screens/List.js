b9b46873931d8729293e6c9715f05f28887087a8
Alan Buzek <alanbuzek@exosite.com>
2019-12-04 11:49:16 +0100

    ListItem progress

--- frontend/src/screens/List.js
+++ frontend/src/screens/List.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import mockList from '../misc/mock-places'
-import ListItem from '../components/ListItem'; 
+import ListItem from '../components/ListItem'
 
 export default function List() {
-  return mockList.places.map(place => {
-    return (
-      <ListItem {...place} key={place.name} />
-    )
+  const itemsList = mockList.places.map(place => {
+    return <ListItem {...place} key={place.name} />
   })
+
+  return <div style={{ maxWidth: '1200px', margin: 'auto', padding: 10 }}>
+    {itemsList}
+  </div>
 }
