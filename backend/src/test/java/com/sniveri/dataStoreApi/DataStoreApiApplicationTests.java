e1a6fd279c810653eeb3cdc3a297d5592c59c8e3
Lukáš Vévar <lukas.vevar>
2019-12-04 10:04:01 +0100

    Added sample backend

---
+++ backend/src/test/java/com/sniveri/dataStoreApi/DataStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sniveri.dataStoreApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
