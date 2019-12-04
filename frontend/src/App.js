fb4f2c48dc3ee0e8423b9dd7e09d95ff7cf63066
Miroslav Holeček <holecekm@outlook.com>
2019-12-04 10:03:53 +0100

    added routing scaffold

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
+import Home from './screens/Home';
+import List from './screens/List';
+import Details from './screens/Details';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <Router>
+		<Switch>
+			<Route path='/' component={Home} exact />
+			<Route path='/list' component={List} />
+			<Route path='/details/:placeId' component={Details} />
+		</Switch>
+	</Router>
 }
 
 export default App;
